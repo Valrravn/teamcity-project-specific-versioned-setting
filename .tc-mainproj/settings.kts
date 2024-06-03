63da3c8a9b2afb63edeb731da06436a2ae19b1bb
Valravn <Valravn@TeamCity>
2024-06-03 18:39:08 +0200

    TeamCity change in 'Project-Specific Settings' project: Versioned settings configuration updated

---
+++ .tc-mainproj/settings.kts
@@ -0,0 +1,74 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2024.03"
+
+project {
+
+    buildType(BuildAll)
+
+    subProject(SampleProject2)
+    subProject(SampleProject1)
+}
+
+object BuildAll : BuildType({
+    name = "Build All"
+
+    type = BuildTypeSettings.Type.COMPOSITE
+
+    vcs {
+        root(DslContext.settingsRoot)
+
+        showDependenciesChanges = true
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+
+    dependencies {
+        snapshot(RelativeId("Sample1Project_BuildSample1")) {
+        }
+        snapshot(RelativeId("Sample2Project_BuildSample2")) {
+        }
+    }
+})
+
+
+object SampleProject1 : Project({
+    name = "Sample 1 Project"
+})
+
+
+object SampleProject2 : Project({
+    name = "Sample 2 Project"
+})
