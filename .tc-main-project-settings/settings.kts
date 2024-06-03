5996aed6fa4a28266be3d3e4f4b54fafc4e8ef81
Valravn <Valravn@TeamCity>
2024-06-03 15:32:45 +0200

    TeamCity change in 'Teamcity Project Specific Versioned Setting' project: 'Build All' build configuration was created

--- .tc-main-project-settings/settings.kts
+++ .tc-main-project-settings/settings.kts
@@ -25,4 +25,10 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2024.03"
 
 project {
+
+    buildType(BuildAll)
 }
+
+object BuildAll : BuildType({
+    name = "Build All"
+})
